e3a6735271746ea62b186d77df91e2a4515aec18
Caleb Hart <caleb_hart11@yahoo.com>
2023-11-17 19:53:16 -0800

    updated opacity on navbar

--- app/components/MenuOverlay.js
+++ app/components/MenuOverlay.js
@@ -11,6 +11,6 @@ const MenuOverlay = ({ links }) => {
         ))}
     </ul>
   )
-}
+} 
 
 export default MenuOverlay;
--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -24,7 +24,7 @@ const navLinks = [
 export default function NavBar() {
     const [navbarOpen, setNavbarOpen] =useState(false);
   return (
-    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
     <div className=" flex flex-wrap items-center justify-between mx-auto px-4">
       <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold"><span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-emerald-500 to bg-pink-600'>Calebhart.dev</span></Link>
       <div className="mobile-menu block md:hidden">
